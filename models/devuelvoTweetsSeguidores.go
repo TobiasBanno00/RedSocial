036b8760517bb24262fe59a118b93b2751cfaa80
TobiasBanno <tobiasbanno@gmail.com>
2022-12-28 16:42:39 -0300

    correcion errores

--- models/devuelvoTweetsSeguidores.go
+++ models/devuelvoTweetsSeguidores.go
@@ -10,7 +10,7 @@ type DevuelvoTweetsSeguidores struct {
 	ID                primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
 	UsuarioID         string             `bson:"usuarioid" json:"userId,omitempty"`
 	UsuarioRelacionID string             `bson:"usuariorelacionid" json:"userRelationId,omitempty"`
-	Tweer             struct {
+	Tweet             struct {
 		Mensaje string    `bson:"mensaje" json:"mensaje,omitempty"`
 		Fecha   time.Time `bson:"fecha" json:"fecha,omitempty"`
 		ID      string    `bson:"_id" json:"_id,omitempty"`
